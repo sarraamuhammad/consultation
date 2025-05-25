332eb7bba7edad4d2d55c81aa1199878cf8a5498
Sarraa Muhammad <102469790+sarraamuhammad@users.noreply.github.com>
2025-05-25 22:52:49 +0300

    Update matchController.js

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -2,19 +2,6 @@
 const Match = require("../models/match");
 const Stadium = require("../models/stadium");
 
-// const getAllMatches = (req, res, next) => {
-//   Match.find()
-//     .then((matches) => {
-//       console.log(matches);
-//       res.status(200).json(matches);
-//     })
-//     .catch((err) => {
-//       console.log("Couldn't get matches");
-//       console.log(err);
-//       res.status(500).json({ error: "Internal Server Error" });
-//     });
-// };
-//Get All Matches
 const getAllMatches = (req, res, next) => {
   // Get today's date at the start of the day
   const today = new Date();
@@ -160,35 +147,6 @@ const updateMatchValidate = (req, res, next) => {
     });
 };
 
-//Update Match Info
-// const updateMatch = (req, res, next) => {
-//   const matchId = req.body.id;
-//   const homeTeam = req.body.homeTeam;
-//   const awayTeam = req.body.awayTeam;
-//   const matchVenue = req.body.matchVenue;
-//   const dateAndTime = req.body.dateAndTime;
-//   const mainReferee = req.body.mainReferee;
-//   const linesMen = req.body.linesMen;
-//   Match.findOne({ _id: matchId }).then((match) => {
-//     match.homeTeam = homeTeam;
-//     match.awayTeam = awayTeam;
-//     match.matchVenue = matchVenue;
-//     match.dateAndTime = dateAndTime;
-//     match.mainReferee = mainReferee;
-//     match.linesMen = linesMen;
-//     return match
-//       .save()
-//       .then((result) => {
-//         console.log("Match Details Updated Successfully");
-//         console.log(result);
-//         res.status(200).json({ result, message: "Edited Match Successfully" });
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         res.status(500).json({ err, message: "Match Not Updated" });
-//       });
-//   });
-// };
 const updateMatch = async (req, res, next) => {
   try {
     const matchId = req.body.id;
